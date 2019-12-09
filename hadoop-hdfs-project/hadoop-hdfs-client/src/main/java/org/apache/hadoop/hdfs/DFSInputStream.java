79f8583a0aa55acbe71ccb72d726f18dca26c95a
Steve Loughran <stevel@cloudera.com>
2019-12-09 18:51:55 +0000

    HDFS-15042 Add more tests for ByteBufferPositionedReadable.
    Changes
    
    * Javadocs in ByteBufferPositionedReadable are a bit stricter.
    * switch to parameterized JUnit test runs for on/off heap buffers
    * factor put main assertions into higher level asserts
    
    New tests
    * negative offset for reads of actual/empty buffers
    * read position > EOF.
    * empty files
    * verify that interleaving simple reads/seeks work.
    
    Change-Id: I27294b15f7cad8e1b87150d9e9fac0623fc44c45

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
@@ -92,6 +92,7 @@ import com.google.common.annotations.VisibleForTesting;
 
 import javax.annotation.Nonnull;
 
+import static org.apache.hadoop.fs.FSExceptionMessages.NEGATIVE_POSITION_READ;
 import static org.apache.hadoop.hdfs.util.IOUtilsClient.updateReadStatistics;
 
 /****************************************************************
@@ -1718,6 +1719,9 @@ public class DFSInputStream extends FSInputStream
   @Override
   public void readFully(long position, final ByteBuffer buf)
       throws IOException {
+    if (position < 0) {
+      throw new EOFException(NEGATIVE_POSITION_READ);
+    }
     int nread = 0;
     while (buf.hasRemaining()) {
       int nbytes = read(position + nread, buf);
